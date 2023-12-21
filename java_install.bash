140a50355b944412ca55f6f2aae39140f4b24fff
Alvin-He <zhongjihe.alvin@gmail.com>
2023-12-21 06:42:20 -0800

    fail safe for when I accidentely delete the mkdir in dockerfile

--- java_install.bash
+++ java_install.bash
@@ -14,5 +14,5 @@ apt-get install -y --no-install-recommends openjdk-$JAVA_REL-jdk-headless
 (javac -version | grep -q "javac $JAVA_REL.*") || exit
 
 JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' | grep -oh '\/.*\w') 
-ln -s $JAVA_HOME "/home/system/wpilib/$COMP_YEAR/jdk"
+mkdir -p /home/system/wpilib/$COMP_YEAR/jdk && ln -s $JAVA_HOME "/home/system/wpilib/$COMP_YEAR/jdk"
 echo "Java installed at $JAVA_HOME" 
